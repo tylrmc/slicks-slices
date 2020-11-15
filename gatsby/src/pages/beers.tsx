a67aee1f7dd6bb54bb5ce8fdb926075b36746928
Tyler McConnell <tylrmac21@gmail.com>
2020-11-15 15:42:43 -0500

    Lecture 5

--- gatsby/src/pages/beers.tsx
+++ gatsby/src/pages/beers.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Nav } from '../components/Nav';
 
 class BeersPage extends React.Component {
   render() {
     return (
       <div>
+        <Nav />
         <p>Hey! I'm the beers page.</p>
       </div>
     );
--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Nav } from '../components/Nav';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
+        <Nav />
         <p>Hey! I'm the home page.</p>
       </div>
     );
